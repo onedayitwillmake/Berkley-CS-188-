25ce66a8cca1ecd3ba8b520724fac0e195257866
onedayitwillmake <mariogonzalez@gmail.com>
2011-11-03 19:17:50 -0400

    Initial commit

---
+++ Homework1/Operators.py
@@ -0,0 +1,6 @@
+mystring = "aritifcial" + "intelligence"
+
+print( help(mystring.find) )
+
+if __name__ == "__main__": print("one.py is being run directly")
+else: print("one.py is being imported into another module")
